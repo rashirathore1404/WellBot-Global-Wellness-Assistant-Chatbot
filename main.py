06e45695d456721c539adfb3aba4126d1594446f
rashirathore1404 <rashirathore1404@gmail.com>
2026-02-07 18:02:35 +0530

    API's (flask and fast)

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to FastAPI"}
+
+@app.post("/add")
+def add_numbers(a: int, b: int):
+    return {"sum": a + b}
